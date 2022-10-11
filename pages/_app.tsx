e270f9f827e7e3719021a3a6af60794816e454b9
morphalex90 <piero.nanni@gmail.com>
2022-10-11 21:01:48 +0100

    add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '@css/index.scss';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', '${process.env.NEXT_PUBLIC_GA}', 'auto');
+          ga('set', 'anonymizeIp', true);
+          ga('send', 'pageview');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
+
 }
 
 export default MyApp
