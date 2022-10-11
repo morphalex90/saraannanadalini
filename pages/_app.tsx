68ac62ba6f35d840caf333e214709973a688bd89
morphalex90 <piero.nanni@gmail.com>
2022-10-11 21:33:51 +0100

    convert to use GA4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', '${process.env.NEXT_PUBLIC_GA}', 'auto');
-          ga('set', 'anonymizeIp', true);
-          ga('send', 'pageview');
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id='${process.env.NEXT_PUBLIC_GA}'"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${process.env.NEXT_PUBLIC_GA}', { 'anonymize_ip': true });
+          </script>
         `}
       </Script>
       <Component {...pageProps} />
