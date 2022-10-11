c777e9de62672c1946a8aff321ac7cb4a48c0a23
morphalex90 <piero.nanni@gmail.com>
2022-10-11 20:50:08 +0100

    add api to send emails

---
+++ pages/api/send-email.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import sgMail from '@sendgrid/mail';
+
+const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
+        const { name, email, message } = req.body;
+
+        const msg = {
+            to: 'saraannanadalini@gmail.com',
+            from: name + ' <info@saraannanadalini.com>', // Use the email address or domain you verified above
+            replyTo: email,
+            subject: 'New contact from ' + name + ' <' + email + '>',
+            text: message,
+            html: message,
+        };
+
+        return sgMail
+            .send(msg)
+            .then((sent) => {
+                console.log(sent);
+                return res.json({ success: true });
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json({ error: err.message, success: false });
+            });
+    }
+
+    return res.status(404).json({
+        error: {
+            success: false,
+            code: 'not_found',
+            message: "The requested endpoint was not found or doesn't support this method."
+        }
+    });
+}
+
+export default sendEmail;
