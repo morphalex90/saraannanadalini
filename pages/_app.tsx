12905e46d23841e3e6e3e39d6a8465f90f29be47
morphalex90 <piero.nanni@gmail.com>
2022-10-11 21:36:25 +0100

    correct use of GA4 script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,17 +5,13 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id='${process.env.NEXT_PUBLIC_GA}'"></Script>
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-          <!-- Google tag (gtag.js) -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id='${process.env.NEXT_PUBLIC_GA}'"></script>
-          <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-          
             gtag('config', '${process.env.NEXT_PUBLIC_GA}', { 'anonymize_ip': true });
-          </script>
         `}
       </Script>
       <Component {...pageProps} />
